bddd8655da3cdd2c2891dad3e83e6c876da9fb8a
duartedaniel92 <duartedaniel473@gmail.com>
2025-06-10 01:56:57 +0000

    Nível Intermediário

--- xadrez.c
+++ xadrez.c
@@ -7,44 +7,56 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-    int bispo = 1, rainha = 1;
+    int bispo = 1, rainha = 1, cavalo = 1;
 
     printf("Desafio de Xadrez - MateCheck!\n");
-
+    printf("\n");
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
     printf("Movimentação Bispo\n");
-
+    printf("\n");
     while (bispo <= 5)
     {
         printf("Cima\n");
         printf("Direita\n");
         bispo++;
     }
-    
+    printf("\n");
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
     printf("Movimentação Torre\n");
-
+    printf("\n");
     for (int torre = 0; torre < 5; torre++)
     {
         printf("Direita\n");
     }
-    
+    printf("\n");
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
     printf("Movimentação Rainha\n");
-
+    printf("\n");
     do
     {
         printf("Esquerda\n");
         rainha++;
     } while (rainha <= 8);
-    
+    printf("\n");
+
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
-
+    printf("Movimentação Cavalo\n");
+    printf("\n");
+    while (cavalo--)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            printf("Cima\n");
+        }
+        printf("Direita\n");
+    }
+    printf("\n");
+    
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.
